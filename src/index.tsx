7acb7fdbd652dab328c2b9f74122ff00082d7cf5
DaniAkash <s.daniakash@gmail.com>
2020-01-30 05:21:49 +0530

    project created

---
+++ src/index.tsx
@@ -0,0 +1,60 @@
+import React, { ReactNode } from "react";
+import {
+  ScrollView,
+  ScrollViewProps,
+  View,
+  StyleProp,
+  ViewStyle
+} from "react-native";
+
+export interface RNMasonryScrollViewProps extends ScrollViewProps {
+  children: ReactNode[];
+  columns?: number;
+  columnStyle?: StyleProp<ViewStyle>;
+  oddColumnStyle?: StyleProp<ViewStyle>;
+  evenColumnStyle?: StyleProp<ViewStyle>;
+}
+
+function generateMasonryGrid<T>(data: T[]): T[][] {
+  return data.reduce((collection: T[][], child: T, childIndex: number) => {
+    const itemIndex = childIndex % 4;
+    if (collection[itemIndex]) {
+      collection[itemIndex].push(child);
+    } else {
+      collection[itemIndex] = [];
+      collection[itemIndex].push(child);
+    }
+    return collection;
+  }, []);
+}
+
+const RNMasonryScrollView = ({
+  children = [],
+  columns = 2,
+  columnStyle = null,
+  oddColumnStyle = null,
+  evenColumnStyle = null,
+  ...otherProps
+}: RNMasonryScrollViewProps) => {
+  const masonryGrid = generateMasonryGrid(children);
+
+  return (
+    <ScrollView {...otherProps}>
+      {masonryGrid.map((column, columnIndex) => {
+        return (
+          <View
+            key={columnIndex}
+            style={[
+              columnStyle,
+              columnIndex % 2 === 0 ? evenColumnStyle : oddColumnStyle
+            ]}
+          >
+            {column.map(item => item)}
+          </View>
+        );
+      })}
+    </ScrollView>
+  );
+};
+
+export default RNMasonryScrollView;
